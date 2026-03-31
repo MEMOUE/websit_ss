c593b157dcd9eefa5bbcf53d2086da6c60f2b936
Memko <memko021@gmail.com>
2026-03-31 22:18:50 +0000

    fix changement de page facebook

--- website_ss/settings.py
+++ website_ss/settings.py
@@ -54,10 +54,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'website_ss.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'website_ss',   # Name of the database you created
+        'USER': 'root',        # Your MySQL username (e.g., 'root')
+        'PASSWORD': '123456789',    # Your MySQL password
+        'HOST': 'localhost',            # Or the IP address of your MySQL server
+        'PORT': '3306',                 # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
